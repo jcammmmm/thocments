8711224090001ac348eb993b23fbe1fa0d9db79e
Juan Camilo Florez <jcflorezv@unal.edu.co>
2023-09-07 11:05:44 -0500

    added JUnit sample

---
+++ thocs/java-interview/code/utest/src/main/java/sampl/jacammmmm/App.java
@@ -0,0 +1,23 @@
+package sampl.jacammmmm;
+
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+public class App {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+    public static void main(String... args) {
+        System.out.println("buahhahahaah!");
+    }
+    
+}
---
+++ thocs/java-interview/code/utest/src/test/java/sampl/jacammmmm/AppTest.java
@@ -0,0 +1,31 @@
+package sampl.jacammmmm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class AppTest {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		App calculator = new App();
+		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+			"0,    1,   1",
+			"1,    2,   3",
+			"49,  51, 100",
+			"1,  100, 101"
+	})
+	void add(int first, int second, int expectedResult) {
+		App calculator = new App();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " + " + second + " should equal " + expectedResult);
+	}
+}
